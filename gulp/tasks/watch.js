bf036077f8a4770102b8b8c2c35ee702db77f406
marinik17 <marinik08@gmail.com>
2018-04-29 14:18:17 +0200

    Finnished Mobile-Menu section JS

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,24 +1,35 @@
 var gulp = require('gulp'),
- watch = require('gulp-watch'),
- browserSync = require('browser-sync').create();
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
 
   browserSync.init({
-    notify:false,
-    server:{
-      baseDir:"app"
+    notify: false,
+    server: {
+      baseDir: "app"
     }
   });
 
-  watch('./app/index.html', function(){
+  watch('./app/index.html', function() {
     browserSync.reload();
   });
-  watch('./app/assets/styles/**/*.css', function(){
+
+  watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
   });
+
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
+
 });
-gulp.task('cssInject',['styles'], function(){
-  gulp.src('./app/temp/styles/styles.css')
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
 });
